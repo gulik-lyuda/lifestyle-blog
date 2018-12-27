aaba3e6e6a91fc8e346e11adaa053216bd212220
Lyudmyla Gulik <lyudmyla_gulik@Lyudmylas-Mac-mini.local>
2018-12-28 00:16:56 +0200

    Add the Slider section markup

---
+++ source/js/main.js
@@ -0,0 +1,37 @@
+(function () {
+    "use strict";
+// working with a slider  
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    document.getElementById("prev").onclick = sliderLeft;
+    document.getElementById("next").onclick = sliderRight;
+
+    function sliderLeft() {
+        showSlides(slideIndex-1);
+    }
+
+    function sliderRight() {
+        showSlides(slideIndex+1);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("slides");
+
+        if (n > slides.length) {
+            slideIndex = 1
+        } else if (n < 1) {
+            slideIndex = slides.length
+        } else {
+            slideIndex = n 
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+
+        slides[slideIndex - 1].style.display = "block";  
+    }
+// end working with a slider
+})();
